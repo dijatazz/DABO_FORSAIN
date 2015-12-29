abf72f45a7594316f15ef1c26738a5688a3ba143
FORSAIN <jeanluc.forsain@gmail.com>
2015-12-29 22:43:43 +0100

    Nineteenth commit - MaJ bundle - 29/12/2015

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,8 +2,7 @@ class Post < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :dish
 
-	validates :content, :presence => true, :length => { :maximum => 140 }
-	validates :user_id, :presence => true
+	
 
-	default_scope :order => 'posts.created_at DESC'
+	
 end
